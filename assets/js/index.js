bdb66eb7886057fcfdee0867cf1f94b09cef8a66
Ygor-CCarvalho <ygorcdc@gmail.com>
2024-04-01 23:36:45 -0300

    css e html finalizados

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const outputYear = document.querySelector(".output-year");
+const outputMonth = document.querySelector(".output-month");
+const outputDay = document.querySelector(".output-day");
+const submitButton = document.querySelector(".submit-btn");
+
+//input
+
+const inputYear = document.querySelector("#year");
+const inputMonth = document.querySelector("#month");
+const inputDay = document.querySelector("#day");
+
+
+
